18fcc02a312b78c5a4a733b4d2aa024218c45cb0
Bismus <alexeybismus@gmail.com>
2025-10-15 20:14:39 +0300

    wip: add mock for review

--- src/types/reviewTypes.ts
+++ src/types/reviewTypes.ts
@@ -1,12 +1,14 @@
 export type Review = {
   id: string;
-  user: {
-    name: string;
-    avatarUrl: string;
-  };
+  user: user;
   rating: number;
   comment: string;
   date: string;
+};
+
+export type user = {
+  name: string;
+  avatarUrl: string;
 };
 
 export type Reviews = Review[];
