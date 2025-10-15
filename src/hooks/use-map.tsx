7d82abc18e8ef77834372929053030bcd2052a61
Bismus <alexeybismus@gmail.com>
2025-10-15 17:04:44 +0300

    wip: add map hook

---
+++ src/hooks/use-map.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState, useRef, MutableRefObject } from 'react';
+import { Map, TileLayer } from 'leaflet';
+import { City } from '../types/offersTypes';
+
+function useMap(
+  mapRef: MutableRefObject<HTMLElement | null>,
+  city: City
+): Map | null {
+  const [map, setMap] = useState<Map | null>(null);
+  const isRenderedRef = useRef(false);
+
+  useEffect(() => {
+    if (mapRef.current && !isRenderedRef.current) {
+      const instance = new Map(mapRef.current, {
+        center: { lat: city.lat, lng: city.lng },
+        zoom: 10,
+      });
+
+      const layer = new TileLayer(
+        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        }
+      );
+
+      instance.addLayer(layer);
+      setMap(instance);
+      isRenderedRef.current = true;
+    }
+  }, [mapRef, city]);
+
+  useEffect(() => {
+    if (map) {
+      map.setView({ lat: city.lat, lng: city.lng }, 10);
+    }
+  }, [map, city]);
+
+  return map;
+}
+
+export default useMap;
