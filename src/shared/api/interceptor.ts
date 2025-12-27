23cc4d409b39814103715ba3af594d0f1934f863
Bismus <alexeybismus@gmail.com>
2025-12-27 13:10:12 +0300

    test: iterceptor

--- src/shared/api/interceptor.ts
+++ src/shared/api/interceptor.ts
@@ -1,36 +1,36 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 import { getAccessToken, removeAccessToken } from './authToken';
 import { AppRoute } from '../../consts';
 
 const PUBLIC_ROUTES = [AppRoute.Login, AppRoute.Root];
 
-export function setupInterceptors(api: AxiosInstance) {
-  api.interceptors.request.use((config) => {
-    const token = getAccessToken();
+export function requestInterceptor(config: InternalAxiosRequestConfig) {
+  const token = getAccessToken();
 
-    if (token && config.headers) {
-      config.headers['X-Token'] = token;
-    }
+  if (token && config.headers) {
+    config.headers['X-Token'] = token;
+  }
 
-    return config;
-  });
+  return config;
+}
 
-  api.interceptors.response.use(
-    (response) => response,
-    (error: unknown) => {
-      if (axios.isAxiosError(error)) {
-        if (
-          error.response?.status === 401 &&
-          !PUBLIC_ROUTES.includes(window.location.pathname as AppRoute)
-        ) {
-          removeAccessToken();
-          if (window.location.pathname !== (AppRoute.Login as string)) {
-            window.location.href = AppRoute.Login;
-          }
-        }
+export function responseInterceptor(error: unknown) {
+  if (axios.isAxiosError(error)) {
+    if (
+      error.response?.status === 401 &&
+      !PUBLIC_ROUTES.includes(window.location.pathname as AppRoute)
+    ) {
+      removeAccessToken();
+      if (window.location.pathname !== (AppRoute.Login as string)) {
+        window.location.href = AppRoute.Login;
       }
-
-      return Promise.reject(error);
     }
-  );
+  }
+
+  return Promise.reject(error);
+}
+
+export function setupInterceptors(api: AxiosInstance) {
+  api.interceptors.request.use(requestInterceptor);
+  api.interceptors.response.use((res) => res, responseInterceptor);
 }
